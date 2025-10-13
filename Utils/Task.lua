affbabb2f6c157401c7b0fff8abee7a8e4d5f1e4
0xf1c40f <lbrthc@gmail.com>
2025-10-13 16:38:02 +0700

    Update Task.lua

--- Utils/Task.lua
+++ Utils/Task.lua
@@ -1,16 +1,20 @@
+local RunService = game:GetService("RunService")
 local Connections = {}
 local ModuleReturn = {}
+
 function ModuleReturn:Start(func)
-  table.insert(Connections, RunService.Heartbeat:Connect(func))
+	table.insert(Connections, RunService.Heartbeat:Connect(func))
 end
-  
+
 function ModuleReturn:End(func)
-for i,v in Connections do
-v:Disconnect()
+	for i, v in ipairs(Connections) do
+		v:Disconnect()
+	end
+	table.clear(Connections)
+	if func then
+		task.spawn(func)
+	end
 end
-  if func then
-  task.spawn(func)
-end
-end
+
 shared.Task = ModuleReturn
 return ModuleReturn
