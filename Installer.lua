a25e162f4dad7ef1a0c770ba5790e423203c0f53
0xf1c40f <lbrthc@gmail.com>
2025-10-17 16:13:27 +0700

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -1,1 +1,19 @@
-for k,v in
+-- loader/installer hhhhh :joy:
+local get   = game.HttpGet
+local id    = game.PlaceId
+local dir   = 'localhook.win'
+local repo  = 'https://raw.githubusercontent.com/0xf1c40f/localhook.win/main/Games/'
+
+if not isfolder(dir) then
+makefolder(dir)
+end
+
+local file = dir .. '/' .. id .. '.lua'
+local ok, src = pcall(get, repo .. id .. '.lua')
+
+if ok and src and #src > 0 then
+  if not isfile(file) then 
+    writefile(file, src) 
+  end
+    loadfile(file)()
+end
